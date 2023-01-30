0dc653f0e99d77d395f0d9b81d03aefc3efbedd0
Mohibul Alom <malom@flexicar.es>
2023-01-30 16:26:20 +0100

    feat: add table component

---
+++ src/component/Table.jsx
@@ -0,0 +1,33 @@
+import React from 'react'
+import { getValueByKeys } from '../utils/getValueByKeys'
+import './style.css'
+
+const Table = ({
+    columns = [],
+    dataSource = []
+}) => {
+  return (
+    <table>
+      <thead>
+          <tr>
+            {columns.map(column => (
+              <th key={column.key}>{column.title}</th>
+            ))}
+          </tr>
+        </thead>
+        <tbody>
+          {dataSource.map((data, index) => (
+            <tr key={index}>
+              {columns.map(dataColumn => (
+                <td data-column={dataColumn.title} key={dataColumn.key}>
+                  {getValueByKeys(data, dataColumn.dataIndex)}
+                </td>
+              ))}
+            </tr>
+          ))}
+        </tbody>
+    </table>
+  )
+}
+
+export default React.memo(Table)
