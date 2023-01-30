c8c997585e574a369ff246cb12dc87c8dc250859
Mohibul Alom <malom@flexicar.es>
2023-01-30 16:27:02 +0100

    feat: add function to get value with nested key

---
+++ src/utils/getValueByKeys.js
@@ -0,0 +1,16 @@
+export function getValueByKeys(
+    entity,
+    path,
+  ) {
+    let current = entity;
+  
+    for (let i = 0; i < path.length; i += 1) {
+      if (current === null || current === undefined) {
+        return undefined;
+      }
+  
+      current = current[path[i]];
+    }
+  
+    return current;
+  }
